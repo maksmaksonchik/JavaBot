66255ce38bfbcd19f594bc12fa6081df5dcb50b8
m.y.efremov <m.y.efremov@tinkoff.ru>
2024-09-24 00:41:21 +0500

    feat(task-1): implements echo bot

---
+++ src/main/java/ru/urfu/Bot.java
@@ -0,0 +1,37 @@
+package ru.urfu;
+
+import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
+import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
+
+public class Bot implements LongPollingSingleThreadUpdateConsumer {
+    private final TelegramClient telegramClient;
+
+    public Bot(String token) {
+        telegramClient = new OkHttpTelegramClient(token);
+    }
+
+    @Override
+    public void consume(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            String answerText = String.format("Ты написал:\n\n\"%s\"", messageText);
+
+            SendMessage message = SendMessage
+                    .builder()
+                    .chatId(chatId)
+                    .text(answerText)
+                    .build();
+            try {
+                telegramClient.execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- src/main/java/ru/urfu/Main.java
+++ src/main/java/ru/urfu/Main.java
@@ -1,10 +1,20 @@
 package ru.urfu;
 
-/**
- * Основной класс для запуска приложения
- */
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String botToken = System.getenv("BOT_TOKEN");
+
+        try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
+            botsApplication.registerBot(botToken, new Bot(botToken));
+            System.out.println("Bot successfully started!");
+
+            Thread.currentThread().join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
